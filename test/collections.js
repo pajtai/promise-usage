72d5a5cd12818a304f7225632c68e96298a39dbe
Peter Ajtai <peter.ajtai@thinksolid.com>
2015-03-02 12:47:56 -0800

    adding more tests

---
+++ test/collections.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var chai = require('chai'),
+    BB = require('bluebird');
+
+chai.should();
+
+describe('collections', function() {
+
+    it('can resolve with an object made up of promise values', function(done) {
+    
+        BB
+            .props({
+                test1: waitResolve(3, 'a'),
+                test2: 5,
+                test3: waitResolve(5, 'b')
+            })
+            .then(function(obj) {
+            
+                obj.should.deep.equal({
+                    test1: 'a',
+                    test2: 5,
+                    test3: 'b'
+                });
+                done();
+            })
+            .catch(done);
+    });
+});
+
+function waitResolve(time, value) {
+    return new BB(function(resolve) {
+        setTimeout(function() {
+            resolve(value);
+        }, time);
+    });
+}
--- test/series.js
+++ test/series.js
@@ -21,7 +21,7 @@ describe('Running functions in series', function() {
     it('can start a promise chain with a synchronous method using resolve', function(done) {
 
         BB
-            .resolve(returnFive())
+            .try(returnFive)
             .then(function(fiver) {
                 fiver.should.equal(5);
                 //6..should.equal(2);
@@ -29,7 +29,63 @@ describe('Running functions in series', function() {
             .then(done)
             .catch(done);
     });
+
+    it('can start a promise chain with a value', function(done) {
+        BB
+            .resolve(5)
+            .then(function(fiver) {
+                fiver.should.equal(5);
+            })
+            .then(done)
+            .catch(done);
+        
+    });
+
+    it('can start a promise chain with a q promise', function(done) {
+        var deferred = require('q').defer();
+
+        BB
+            .resolve(deferred.promise)
+            .then(function(fiver) {
+                fiver.should.equal(5);
+            })
+            .then(done)
+            .catch(done);
+
+
+        deferred.resolve(5);
+    });
+
+    it('cannot handle errors in methods passed to resolve', function(done) {
+    
+        try {
+            BB.resolve(errorProne({}));
+        } catch(e) {
+            // expext and unhandled error here:
+            done();
+        }
+
+    });
+
+    it('can handle errors in methods passed to try', function(done) {
+        
+        BB
+            .try(function() {
+                return errorProne({some:'example'});
+            })
+            .then(function() {
+                done('should not get here');
+            })
+            .catch(function(e) {
+                e.message.should.equal("Cannot read property 'not' of undefined");
+                done();
+            });
+    });
 });
+
+        function errorProne(user) {
+            return 5 === user.does.not.exist;
+        }
 
 function returnFive() {
     return 5;
